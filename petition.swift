f0dd6d83857a7af5c240716e0ca009db4eba5710
Worrell17 <40580559+Worrell17@users.noreply.github.com>
2019-06-24 22:53:13 +0100

    Add files via upload

---
+++ petition.swift
@@ -0,0 +1,15 @@
+//
+//  petition.swift
+//  Project 7
+//
+//  Created by Richard Worrell on 21/06/2019.
+//  Copyright © 2019 Richard Worrell. All rights reserved.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var name: String
+    var description: String
+    //var signatureCount: Int
+}
